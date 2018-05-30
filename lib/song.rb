78c35df228bcb6ff0baf7dee4ced08cdf091e700
Matt Farmer <mrfarmer777@gmail.com>
2018-05-30 04:38:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,13 @@ class Song
     @artist=nil
   end
 
+  def new_by_filename(filename)
+    art_name=filename.split(" - ")[0]
+    song_name=filename.split(" - ")[1]
+    song=Song.new(song_name)
+    song.artist=art_name
+    song
+  end
 end
 
     
